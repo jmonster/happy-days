2de43a1efa1ff6cfc7e86412b542d5922d811553
Johnny Domino <jvdomino@gmail.com>
2016-03-20 13:20:53 -0700

    look for existing user record on login

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,12 +4,15 @@ export default Ember.Route.extend({
   authenticator: 'authenticator:torii',
   actions: {
     authenticate: function(provider){
+      Ember.Logger.info(`authenticate:provider: ${provider}`);
+
       this.get('session')
         .open('google-token')
         .then(({ currentUser }) => {
+          Ember.Logger.info(`authenticate:currentUser: ${JSON.stringify(currentUser)}`);
           /* nada */
         }, (error) => {
-          console.error(error);
+          console.error(`authenticate:error: ${error}`);
         });
     }
   }
--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -3,22 +3,25 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   store: Ember.inject.service(),
   open: function(authentication){
+    Ember.Logger.info(`authentication: ${JSON.stringify(authentication)}`);
 
-  // look for record on server
-    // todo
+    // look for record on server
+    return this.get('store')
+      .queryRecord('user', { filter: { email: authentication.userEmail } })
+      .then((user) => {
+        Ember.Logger.info(`open: authentication.provider: ${authentication.provider}`);
 
-  // or
+        if (Ember.isEmpty(user)) {
+          user = this.get('store').createRecord('user', {
+            access_token: authentication.access_token,
+            name: authentication.userName,
+            email: authentication.userEmail,
+            provider: authentication.provider
+          });
+        }
 
-  // create record on server
-    let user = this.get('store').createRecord('user', {
-      access_token: authentication.access_token,
-      name: authentication.userName,
-      email: authentication.userEmail,
-      provider: authentication.provider
-    });
-
-    return {
-      currentUser: user
-    }
+        Ember.Logger.info(`open:currentUser: ${JSON.stringify(user)}`);
+        return { currentUser: user };
+      });
   }
 });
